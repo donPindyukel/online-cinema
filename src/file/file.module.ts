13ff92c40b1e410fa13ef94055502cac2c05e835
Andrey Pindyuk <a913000@gmail.com>
2023-08-25 18:12:36 +0700

    Test static dir

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common'
 import { FileService } from './file.service'
 import { FileController } from './file.controller'
 import { ServeStaticModule } from '@nestjs/serve-static'
-import { path, resolve } from 'app-root-path'
+import { path } from 'app-root-path'
 import { ConfigModule } from '@nestjs/config'
+import { join } from 'path';
+
+console.log("etert", path,join(__dirname, '/../../../', 'uploads'));
 
 @Module({
 	imports: [
 		ServeStaticModule.forRoot({
-			rootPath:  `${resolve('/..')}/uploads`,
-			serveRoot: '/uploads',
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
 		}),
 		ConfigModule,
 	],
