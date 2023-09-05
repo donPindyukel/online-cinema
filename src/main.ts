5642f05ccf1178216702820272ce9c200a2bd712
Andrey Pindyuk <a913000@gmail.com>
2023-09-05 22:52:09 +0700

    Add cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
-  app.enableCors()
+  app.enableCors( { 
+    origin: ['https://movieflix-lemon.vercel.app/', 'https://www.google.com'],
+  })
   await app.listen(4200)
 }
 bootstrap()
