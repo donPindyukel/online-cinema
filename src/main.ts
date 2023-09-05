d4b6a92292662351b3f6aafd5d73791f2e41fc99
Andrey Pindyuk <a913000@gmail.com>
2023-09-05 23:04:50 +0700

    Remove cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
-  app.enableCors( { 
-    origin: ['https://movieflix-lemon.vercel.app/', 'https://www.google.com'],
-  })
   await app.listen(4200)
 }
 bootstrap()
